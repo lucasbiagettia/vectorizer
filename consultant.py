257d5f6a3031c642d971a0feab54551ebc0254e8
lucasbiagetti <lucasbiagetti@gmail.com>
2024-01-28 01:58:32 -0300

    added streamlit

--- consultant.py
+++ consultant.py
@@ -11,19 +11,16 @@ dbname = 'vectorpoc'
 db_manager = PosgresManager(dbname)
 db_manager.connect()
 table_name = "marx"
-user_inp = ""
 
 
-while True:
+def get_answer():
     user_inp = input("Que quieres saber: ")
-    if user_inp == 'fin':
-        break
     embedding = embedding_model.get_embedding(user_inp)
     sim_docs = db_manager.get_similar_docs(embedding, 5, table_name)
     agent = ConversationalAgent()
     ans = agent.answer_question(user_inp, sim_docs)
     print(ans)
-
-db_manager.close_connection()
+def close():
+    db_manager.close_connection()
 
 
