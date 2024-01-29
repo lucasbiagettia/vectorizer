aab746f92bfad85126dbe4ad069eb1bb799fac5e
lucasbiagetti <lucasbiagetti@gmail.com>
2024-01-29 16:46:42 -0300

    added app3

---
+++ aap3.py
@@ -0,0 +1,10 @@
+class AppManager:
+    document_list = []
+    def add_document(document):
+        return 1
+    def make_question(question):
+        return 1
+    def select_document(document):
+        return 1
+    def get_all_documents():
+        return 1
