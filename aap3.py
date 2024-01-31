9ae972d311dee1e9afd2416a9e1fc2666c96ffcf
lucasbiagetti <lucasbiagetti@gmail.com>
2024-01-31 12:05:00 -0300

    app manager

--- aap3.py
+++ aap3.py
@@ -1,10 +1,31 @@
+from data_processor import TxtProcessor
+from db_manager import PosgresManager
+
+
 class AppManager:
     document_list = []
-    def add_document(document):
-        return 1
-    def make_question(question):
-        return 1
-    def select_document(document):
-        return 1
-    def get_all_documents():
-        return 1
+    def add_document(dbname, document, embedding_model):
+        table_name = document.replace(' ', '_').replace('.', '').replace('-', '').replace('.pdf', '').replace('.txt', '')
+        data_processor = TxtProcessor(document, embedding_model)
+        df = data_processor.get_processed_data()
+        dim = embedding_model.get_hidden_size()
+        db_manager = PosgresManager(dbname)
+        db_manager.connect()
+        db_manager.create_extension()
+        db_manager.create_table(table_name, dim)
+        db_manager.insert_data(df, table_name)
+        db_manager.close_connection()
+
+    def make_question(dbname, table_name, question, embedding_model, inference_model):
+        db_manager = PosgresManager(dbname)
+        db_manager.connect()
+        embedding = embedding_model.get_embedding(question)
+        sim_docs = db_manager.get_similar_docs(embedding, 5, table_name)
+        ans = inference_model.answer_question(question, sim_docs)
+        return ans
+
+    def get_all_tables(dbname):
+        db_manager = PosgresManager(dbname)
+        db_manager.connect()
+        tables = db_manager.get_table_names()
+        return tables
