1b65eb89a8fe6df697133a72028ecac61f76d2d5
lucasbiagetti <lucasbiagetti@gmail.com>
2024-02-03 04:32:36 -0300

    better arch streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,40 @@
 import os
 import streamlit as st
 from app_manager import AppManager
-from inference_model.answer_generator import InferenceModel
+from src.inference_model_manager import InferenceModel
 from src.embedding_model_manager import EmbeddingModel
- 
 
-if "app_manager" not in st.session_state:
-    st.session_state.app_manager = AppManager()
+def initialize_session_variables():
+    if "app_manager" not in st.session_state:
+        st.session_state.app_manager = AppManager()
 
-if "db_name" not in st.session_state:
-    st.session_state.db_name = 'vectorpoc'
+    if "db_name" not in st.session_state:
+        st.session_state.db_name = 'vectorpoc'
 
-if "embedding_model" not in st.session_state:
-    st.session_state.embedding_model = EmbeddingModel()
+    if "inference_model" not in st.session_state:
+        st.session_state.inference_model = None
 
-if "selected_document" not in st.session_state:
-    st.session_state.selected_document = None
+    if "embedding_model" not in st.session_state:
+        st.session_state.embedding_model = EmbeddingModel()
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    if "selected_document" not in st.session_state:
+        st.session_state.selected_document = None
 
-if "selected_model" not in st.session_state:
-    st.session_state.selected_model = "tiiuae/falcon-7b"
+    if "splitter_model" not in st.session_state:
+        st.session_state.splitter_model = None
 
-if "inference_model" not in st.session_state:
-    st.session_state.inference_model = None
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
 
-def reset_values():
-    st.session_state.messages = []
+initialize_session_variables()
 
 
 def handle_chat_input(prompt):
     with st.chat_message("user"):
         st.markdown(prompt)
     if st.session_state.inference_model is None:
-        st.session_state.inference_model = InferenceModel(st.session_state.selected_model)
+        st.session_state.inference_model = InferenceModel()
     with st.chat_message("assistant"):
         db_name = st.session_state.db_name
         document = st.session_state.selected_document
@@ -51,7 +50,7 @@ def add_document(uploaded_file):
     db_name = st.session_state.db_name
     app_manager.add_document(db_name, uploaded_file, st.session_state.embedding_model)
 
-def get_avaible_documents():
+def get_available_documents():
     app_manager = st.session_state.app_manager
     db_name = st.session_state.db_name
     return app_manager.get_all_tables(db_name)
@@ -61,49 +60,73 @@ def print_all_messages(messages):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-def main():
+def display_header():
     st.header("Vas a preguntar")
 
-    with st.sidebar:
-        if st.sidebar.button("Reiniciar"):
-            reset_values()
-
-        options = get_avaible_documents()
-
-        selected_option = st.selectbox('Select an option:', options)
-        st.write('You have selected:', selected_option)
-        if selected_option is not None:
-            st.session_state.selected_document = selected_option
-
-        selected_model = st.text_input("Select a model", value="tiiuae/falcon-7b")
-        st.session_state.selected_model = selected_model
-        st.write('Modelo seleccionado', selected_model)
-
-        embedding_models = ['PlanTL-GOB-ES/roberta-base-bne']
-        embedding_model = st.selectbox('Select an option:', embedding_models)
-        st.write('You have selected:', embedding_model)
-        if selected_option is not None:
-            st.session_state.embedding_model = EmbeddingModel()
-
-        uploaded_file = st.file_uploader('Select a file', type=['pdf'])
-        file_name2 = st.text_input("Ingresa el nombre del archivo:")
-        if st.button("Upload File"):
-            if uploaded_file is not None and file_name2:
-                app_manager = st.session_state.app_manager
-                db_name = st.session_state.db_name
-                app_manager.add_document(db_name, uploaded_file, st.session_state.embedding_model)
-                file_name, file_extension = os.path.splitext(uploaded_file.name)
-                st.write(f'You have uploaded the file: {file_name}')
-            else:
-                st.write('Debes seleccionar un archivo y asignarle nombre')
+def handle_sidebar():
+    display_inference_model_selection()
+    display_embedding_model_selection()
+    display_splitter_selection()
+    display_document_selection()
+    handle_file_upload()
 
 
+def display_inference_model_selection():
+    selected_model = st.text_input("Select a model", value="tiiuae/falcon-7b")
+    st.write('Modelo seleccionado', selected_model)
+    if selected_model is not None:
+        st.session_state.inference_model = InferenceModel(model_id=selected_model)
+
+def display_embedding_model_selection():
+    embedding_models = ['PlanTL-GOB-ES/roberta-base-bne']
+    embedding_model = st.selectbox('Select an option:', embedding_models)
+    st.write('You have selected:', embedding_model)
+    if embedding_model is not None:
+        st.session_state.embedding_model = EmbeddingModel()
+
+def display_splitter_selection():
+    splitter_models = ['lbiagetti splitter']
+    splitter_model = st.selectbox('Select an option:', splitter_models)
+    st.write('You have selected:', splitter_model)
+    if splitter_model is not None:
+        st.session_state.splitter_model = splitter_model
+
+
+def display_document_selection():
+    options = get_available_documents()
+    selected_option = st.selectbox('Select an option:', options)
+    st.write('You have selected:', selected_option)
+    if selected_option is not None:
+        st.session_state.selected_document = selected_option
+
+def handle_file_upload():
+    uploaded_file = st.file_uploader('Select a file', type=['pdf'])
+    file_name2 = st.text_input("Ingresa el nombre del archivo:")
+    if st.button("Upload File"):
+        process_file_upload(uploaded_file, file_name2)
+
+def process_file_upload(uploaded_file, file_name):
+    if uploaded_file is not None and file_name:
+        add_document(uploaded_file)
+        file_name, file_extension = os.path.splitext(uploaded_file.name)
+        st.write(f'You have uploaded the file: {file_name}')
+    else:
+        st.write('Debes seleccionar un archivo y asignarle nombre')
+
+def display_chat_messages():
     print_all_messages(st.session_state.messages)
 
+def main():
+    initialize_session_variables()
+    display_header()
+    with st.sidebar:
+        handle_sidebar()
+    display_chat_messages()
     prompt = st.chat_input("Ayúdame a continuar escribiendo")
     if prompt:
         handle_chat_input(prompt)
 
-
 if __name__ == "__main__":
     main()
+
+
